149b832299b8b19629592547c7635ef6b4c9e158
Abhishek Wankhade <abhishekwankhade8767@gmail.com>
2026-02-23 10:07:30 +0530

    fixed postgres connection for production

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
-   const { Pool } = require('pg');
-   const pool = new Pool({
-    user: 'postgres',         
-    host: 'localhost',       
-    database: 'notes',    
-    password: 'abhi8767',
-    port: 5432
-    });
+const { Pool } = require("pg");
 
-    module.exports = pool;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+pool
+  .connect()
+  .then(() => console.log("✅ PostgreSQL Connected"))
+  .catch((err) => console.error("❌ DB Connection Error:", err));
+
+module.exports = pool;
